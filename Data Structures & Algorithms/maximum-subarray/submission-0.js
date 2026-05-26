7405324bd2570f7ef499c55e5adebd089d74ad61
Kirtivardhan Singh <40534936+ikirtivardhansingh@users.noreply.github.com>
2026-05-26 17:51:04 +0530

    Add: maximum-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxSubArray(nums) {
+    
+    let l = 0,
+        r = 0,
+        ts = 0,
+        max = ts;
+        if(nums.length === 1) return nums[0];
+
+    while (r < nums.length) {
+            ts += nums[r];
+            if(ts < 0){
+                r++;
+                l = r;
+                ts= 0;
+            }
+            else if (ts> max){ 
+                max = ts;
+                r++;
+            }
+
+    }
+
+
+    return max;
+}
+}
