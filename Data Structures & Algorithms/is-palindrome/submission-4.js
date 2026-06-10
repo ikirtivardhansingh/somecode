83c49a48f36e603d7e2beef516359c6c50cc2c5e
Kirtivardhan Singh <40534936+ikirtivardhansingh@users.noreply.github.com>
2026-06-10 07:58:35 +0530

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        s = s.toLowerCase().replace(/[^a-z0-9]/g,"");
+        let l = 0,
+            r = s.length -1;
+        
+        while(l < r){
+            if(s[l] !== s[r])
+                return false;
+            else {
+                l++;
+                r--;
+            }
+        }
+    return true;
+    }
+}
