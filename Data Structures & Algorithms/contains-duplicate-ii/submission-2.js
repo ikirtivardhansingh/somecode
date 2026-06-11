044416d3e64a8bc40a74e2327ff5ee1980d32c8e
Kirtivardhan Singh <40534936+ikirtivardhansingh@users.noreply.github.com>
2026-06-11 10:06:37 +0530

    Add: contains-duplicate-ii - submission-2

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-2.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {boolean}
+     */
+    containsNearbyDuplicate(nums, k) {
+        let l = 0,
+            r = 1;
+
+        while( r < nums.length){
+            if ( nums[l] == nums[r] && r - l <= k){
+                return true;    
+            }
+            else r++;
+        }
+        return false;
+    }
+}
